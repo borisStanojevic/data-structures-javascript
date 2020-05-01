eff3cf10ef791b98741e7672647d0e3b6f41548f
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2020-05-01 13:24:43 +0000

    HashTable

--- hashtable/HashTable.js
+++ hashtable/HashTable.js
@@ -5,9 +5,9 @@ class HashTable {
   }
 
   put(key, value) {
-    const index = this._getHash(key);
+    const index = this._getIndex(key);
 
-    const entry = {key, value};
+    const entry = new Entry(key, value);
 
     if(!this.data[index])
       this.data[index] = [];
@@ -15,12 +15,12 @@ class HashTable {
   }
 
   get(key) {
-    const index = this._getHash(key);
+    const index = this._getIndex(key);
 
     if(!this.data[index])
       return null 
     
-    for(entry of this.data[index])
+    for(const entry of this.data[index])
       if(entry.key === key)
         return entry;
 
@@ -28,25 +28,54 @@ class HashTable {
   }
 
   remove(key) {
-    //TODO
+    const index = this._getIndex(key);
+
+    if(!this.data[index])
+      throw new Error("There is no bucket for the given key.")
+
+    let bucket = this.data[index];
+
+    let removedEntry = {};
+    for(let i = 0; i < bucket.length;i++) {
+      const entry = bucket[i];
+      if(entry.key === key){
+        bucket.splice(i, 1);
+        return entry;
+      }
+    }
+
+    throw new Error("There is no entry for the given key.")
   }
 
   getKeys() {
-    //TODO
+    return this._getPropertyValues("key");
   }
 
   getValues() {
-    //TODO
+    return this._getPropertyValues("value");
   }
 
-  _getHash(key) {
+  _getPropertyValues(property) {
+    const values = [];
+    for(const bucket of this.data)
+      if(bucket)
+        for(const entry of bucket)
+          values.push(entry[property]);
+      
+    return values;
+  }
+
+  _getIndex(key) {
+    return this._createHash(key) % this.data.length;
+  }
+
+  _createHash(key) {
     let hash = 0;
     for(let i = 0; i < key.length; i++)
-      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+      hash = hash + key.charCodeAt(i) * i
 
     return hash;
   }
-
 }
 
 class Entry {
@@ -55,8 +84,15 @@ class Entry {
     this.key = key;
     this.value = value;
   }
+
 }
 
-const hashTable = new HashTable(50);
-hashTable.put("mitar@grand.rs", 99_999);
-hashTable.put("brena@grand.rs", 999_999);
+// const hashTable = new HashTable(10);
+// hashTable.put("mitar@grand.rs", 99_999);
+// hashTable.put("brena@grand.rs", 999_999);
+// hashTable.put("jashar@grand.rs", 9_999_999);
+
+// hashTable.remove("jashar@grand.rs");
+
+// console.log(hashTable.getKeys());
+// console.log(hashTable.getValues());
